fbf9c7ee880586e296fe286776eabc9e02fd7329
Charlotte Spencer <charlottelaspencer@gmail.com>
2015-01-16 12:42:27 +0000

    Ensure IndexView is not returning posts from the future

--- guestbook2/views.py
+++ guestbook2/views.py
@@ -11,8 +11,11 @@ class IndexView(generic.ListView):
     context_object_name = "latest_posts_list"
 
     def get_queryset(Self):
-        """ Return the last 10 submitted posts """
-        return Post.objects.order_by("-date")[:10]
+        """ Return the last 10 submitted posts
+            (not including those to be published in the future)"""
+        return Post.objects.filter(
+            date__lte=timezone.now()
+            ).order_by("-date")[:10]
 
 
 class FormView(generic.TemplateView):
