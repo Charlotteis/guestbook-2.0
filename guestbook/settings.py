6166e48ccf765772ecd13c23e1728d768cffbf1b
Charlotte Spencer <charlottelaspencer@gmail.com>
2015-01-16 19:47:25 +0000

    Heroku Database

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -59,14 +59,14 @@ WSGI_APPLICATION = 'guestbook.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'guestbookdb',
-        'USER': 'guestbook',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'guestbookdb',
+    #     'USER': 'guestbook',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
 }
 
 
@@ -96,8 +96,8 @@ STATICFILES_DIRS = (
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # # Parse database configuration from $DATABASE_URL from Django
-# import dj_database_url
-# DATABASES['default'] = dj_database_url.config()
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
 # honor the "X-Forwarded Proto" header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
