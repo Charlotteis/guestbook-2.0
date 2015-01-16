af9a3c2a3216780059db5f2c54068746c4388dfc
Charlotte Spencer <charlottelaspencer@gmail.com>
2015-01-16 10:43:45 +0000

    Set up templates for guestbook application

--- guestbook2/models.py
+++ guestbook2/models.py
@@ -9,4 +9,4 @@ class Post(models.Model):
     date = models.DateTimeField("date published")
 
     def __str__(self):
-        return "{0} - {1}".format(self.name, self.date)
+        return self.comment
--- guestbook2/views.py
+++ guestbook2/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def index(request):
+    """ Renders the guestbook2/index.html template
+    """
+
+    latest_posts_list = Post.objects.order_by("-date")[:10]
+    context = {"latest_posts_list": latest_posts_list}
+
+    return render(request, "guestbook2/index.html", context)
